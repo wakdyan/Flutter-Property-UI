5fd27cf7173345273715aa4dedda73122fb44816
Wak Dyan <aditya.dyan@yahoo.com>
2019-08-21 09:42:02 +0700

    First commits

---
+++ lib/assets.dart
@@ -0,0 +1,36 @@
+var mediaImages = [
+  'https://i.pinimg.com/564x/ca/56/05/ca5605aa81ce7ab3802a9b0fbcce9890.jpg',
+  'https://cdn.pixabay.com/photo/2017/09/09/18/25/living-room-2732939_960_720.jpg',
+  'https://i.pinimg.com/564x/cb/ee/e0/cbeee09cf6a4fb818992cb901b5d14bd.jpg',
+
+];
+
+var mediaImageNames = [
+  '3D Tour',
+  'Photos',
+  'Furniture'
+];
+
+var residentialImages = [
+  'https://i.pinimg.com/564x/ac/7c/23/ac7c2388d59562a1fd8954d4f04a8d85.jpg',
+  'https://i.pinimg.com/564x/35/55/05/355505bfbd38db3d79290924da03ed1d.jpg',
+  'https://i.pinimg.com/564x/8d/f8/20/8df820435dbf73ac9e4f68bfcf2a6cee.jpg'
+];
+
+var rating = [
+  '4.5',
+  '4.0',
+  '4.7'
+];
+
+var profileImages = [
+  'https://images.unsplash.com/photo-1552673304-23f6ad21aada?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=377&q=80',
+  'https://cdn.pixabay.com/photo/2015/01/08/18/29/entrepreneur-593358_960_720.jpg',
+  'https://cdn.pixabay.com/photo/2016/11/29/09/32/concept-1868728_960_720.jpg'
+];
+
+var profileNames = [
+  'Nulien Renvoye',
+  'Mike Blower',
+  'Sammy'
+];
---
+++ lib/main.dart
@@ -0,0 +1,339 @@
+import 'package:flutter/material.dart';
+import 'package:property_app/assets.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    var background = Container(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width / 5,
+      color: Color(0xFFF6F7FB),
+    );
+
+    var post = CustomScrollView(
+      physics: BouncingScrollPhysics(),
+      slivers: <Widget>[
+        SliverToBoxAdapter(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 15, top: 15, right: 15),
+            child: Stack(
+              children: <Widget>[
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        CircleAvatar(
+                          backgroundImage: NetworkImage(
+                              'https://lh3.googleusercontent.com/-CPZhzDe99QM/VzCeeRDGm7I/AAAAAAAAAXY/LHt9jIibnF8XiTBpTna6m4EYY2Xc28zyACEwYBhgL/w139-h140-p/juggernaut-vector-wallpaper.jpg'),
+                        ),
+                        CircleAvatar(
+                          child: IconButton(
+                            icon: Icon(
+                              Icons.chat_bubble_outline,
+                              size: 20,
+                            ),
+                            onPressed: () {},
+                          ),
+                          foregroundColor: Colors.grey,
+                          backgroundColor: Color(0xFFF1F2F6),
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 20),
+                      child: RichText(
+                        textAlign: TextAlign.start,
+                        text: TextSpan(
+                          text: 'Welcome back Uran, ',
+                          style: TextStyle(
+                              fontSize: 17,
+                              color: Colors.black,
+                              fontWeight: FontWeight.w600,
+                              letterSpacing: .8),
+                          children: [
+                            TextSpan(
+                              text:
+                                  'everything is progressing great. See our detail.',
+                              style: TextStyle(color: Colors.black26),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                Positioned(
+                  top: 0,
+                  right: 0,
+                  child: CircleAvatar(
+                    child: Text(
+                      '3',
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 11,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    radius: 7,
+                    backgroundColor: Color(0xFFFF5F5F),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        SliverToBoxAdapter(child: Divider(height: 35)),
+        SliverToBoxAdapter(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                Text(
+                  'Listing Media',
+                  style: TextStyle(fontSize: 24),
+                ),
+                Text(
+                  'View All',
+                  style: TextStyle(
+                    color: Colors.black26,
+                    fontWeight: FontWeight.w500,
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+        SliverToBoxAdapter(
+          child: Padding(
+            padding: const EdgeInsets.only(left: 15, top: 25),
+            child: Container(
+              height: 230,
+              color: Colors.transparent,
+              child: ListView.builder(
+                physics: BouncingScrollPhysics(),
+                scrollDirection: Axis.horizontal,
+                itemCount: mediaImages.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Padding(
+                    padding: const EdgeInsets.only(right: 15),
+                    child: Stack(
+                      children: <Widget>[
+                        Container(
+                          height: 180,
+                          width: 180,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(12),
+                                topRight: Radius.circular(12)),
+                            image: DecorationImage(
+                                image: NetworkImage(mediaImages[index]),
+                                fit: BoxFit.cover),
+                          ),
+                        ),
+                        Positioned(
+                          bottom: 0,
+                          child: Container(
+                            height: 50,
+                            width: 180,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.only(
+                                bottomLeft: Radius.circular(12),
+                                bottomRight: Radius.circular(12),
+                              ),
+                              color: Colors.white,
+                            ),
+                            child: Center(
+                              child: Text(
+                                mediaImageNames[index],
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    fontSize: 18, fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+        ),
+        SliverList(
+          delegate: SliverChildBuilderDelegate((context, index) {
+            return Padding(
+              padding: const EdgeInsets.only(top: 30),
+              child: Container(
+                height: 360,
+                color: Colors.white,
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 15, top: 15, right: 15),
+                  child: Column(
+                    children: <Widget>[
+                      Row(
+                        // crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          CircleAvatar(
+                            backgroundImage: NetworkImage(profileImages[index]),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(left: 15),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Row(
+                                  children: <Widget>[
+                                    Text(
+                                      profileNames[index],
+                                      style: TextStyle(
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    Padding(
+                                      padding: const EdgeInsets.only(left: 15),
+                                      child: Row(
+                                      children: <Widget>[
+                                        Icon(
+                                          Icons.star,
+                                          color: Colors.orangeAccent,
+                                          size: 18,
+                                        ),
+                                        Text(
+                                          rating[index],
+                                          style: TextStyle(
+                                              color: Colors.orangeAccent),
+                                        )
+                                      ],
+                                  ),
+                                    ),
+                                  ],
+                                ),
+                                Text('Resindetial, Uni/Flat',
+                                    style: TextStyle(fontSize: 12)),
+                                    
+                              ],
+                            ),
+                          ),
+                          Expanded(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: <Widget>[
+                                Container(
+                                  height: 35,
+                                  width: 75,
+                                  decoration: BoxDecoration(
+                                    color: Color(0xFFE4FCEC),
+                                    borderRadius: BorderRadius.circular(5),
+                                  ),
+                                  child: Center(
+                                    child: Text(
+                                      'For Sale',
+                                      textAlign: TextAlign.center,
+                                      style: TextStyle(
+                                          color: Color(0xFF2cca73),
+                                          fontWeight: FontWeight.w500),
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          )
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 25),
+                        child: Container(
+                          height: 280,
+                          width: MediaQuery.of(context).size.width,
+                          decoration: BoxDecoration(
+                              color: Colors.grey,
+                              borderRadius: BorderRadius.circular(10),
+                              image: DecorationImage(
+                                  image: NetworkImage(residentialImages[index]),
+                                  fit: BoxFit.cover)),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            );
+          }, childCount: residentialImages.length),
+        )
+      ],
+    );
+
+    return SafeArea(
+      child: Scaffold(
+          body: Stack(
+            children: <Widget>[
+              background,
+              post,
+            ],
+          ),
+          bottomNavigationBar: BottomNavigationBar(
+            type: BottomNavigationBarType.fixed,
+            selectedIconTheme: IconThemeData(color: Color(0xFF2cca73)),
+            selectedItemColor: Colors.black,
+            selectedFontSize: 10,
+            unselectedItemColor: Colors.black26,
+            iconSize: 26,
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                title: Text('Home'),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.search),
+                title: Text('Search'),
+              ),
+              BottomNavigationBarItem(
+                icon: Container(
+                  height: 43,
+                  width: 43,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Color(0xFF2cca73),
+                      boxShadow: [
+                        BoxShadow(
+                            color: Color(0xFF2cca73),
+                            offset: Offset(0, 3),
+                            blurRadius: 10,
+                            spreadRadius: 1),
+                      ]),
+                  child: Icon(Icons.add, color: Colors.white, size: 30),
+                ),
+                title: Text(
+                  'Add',
+                  style: TextStyle(fontSize: 0),
+                ),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.bookmark_border),
+                title: Text('Saved'),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.menu),
+                title: Text('More'),
+              ),
+            ],
+          )),
+    );
+  }
+}
